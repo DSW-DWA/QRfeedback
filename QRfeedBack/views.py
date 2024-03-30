33e6925ed39b497fa96e89a51f9abb0135ccef04
justpwned <justpwnedfrom@gmail.com>
2024-03-30 18:55:41 +0500

    throw 404 error on unparameterized feedback page

--- QRfeedBack/views.py
+++ QRfeedBack/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponseBadRequest
 
 
 def main_page_view(request):
+    address = request.GET.get('address', '').strip()
+    encrypted_data = request.GET.get('encrypted_data', '').strip()
+    if address == '' or encrypted_data == '':
+        return HttpResponseBadRequest('<h1>Malformed request</h1>')
     return render(request, 'main.html')
+
 
 def submit_form(request):
     return render(request, 'success.html')
