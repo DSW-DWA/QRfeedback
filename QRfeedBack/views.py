f4c1b495b3a1a438794e3303b79a52abf90c0ba5
server <bocharovks@yandex.ru>
2024-03-30 05:38:47 +0500

    do not use cookies for address and session_id extraction

--- QRfeedBack/views.py
+++ QRfeedBack/views.py
@@ -2,16 +2,7 @@ from django.shortcuts import render
 
 
 def main_page_view(request):
-    response = render(request, 'main.html')
-
-    address = request.GET.get('address', '')
-    tg_session_id = request.GET.get('encrypted_data', '')
-    if address != '' and tg_session_id != '':
-        response.set_cookie('address', address)
-        response.set_cookie('tg_session_id', tg_session_id)
-
-    return response
-
+    return render(request, 'main.html')
 
 def submit_form(request):
     return render(request, 'success.html')
