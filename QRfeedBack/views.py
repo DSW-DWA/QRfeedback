cef64ad522d0dd4d92d5ecb574a127b469417996
justpwned <justpwnedfrom@gmail.com>
2024-05-13 15:00:58 +0500

    redirect from main page to /generate as to avoid user confusion

--- QRfeedBack/views.py
+++ QRfeedBack/views.py
@@ -1,12 +1,11 @@
-from django.shortcuts import render
-from django.http import HttpResponseBadRequest
+from django.shortcuts import render, redirect
 
 
 def main_page_view(request):
     address = request.GET.get("address", "").strip()
     encrypted_data = request.GET.get("encrypted_data", "").strip()
     if address == "" or encrypted_data == "":
-        return HttpResponseBadRequest(b"<h1>Malformed request</h1>")
+        return redirect("qr_gen")
     return render(request, "main.html")
 
 
